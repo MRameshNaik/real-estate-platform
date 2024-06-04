5ce463ce8cb82d1f8a9f551af497a9cbc7f55b73
sribhargav1345 <sribhargav1345@gmail.com>
2024-06-04 17:54:49 +0530

    Database setup Completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+
+const mongoDB = require("./db");
+
 require("dotenv").config();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 5000;  // Keeping some default port, if there is no port number on env file
+
+mongoDB();
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
