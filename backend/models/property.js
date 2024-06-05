616ec20a9469b8889266886e8e854debcfc59d30
Anjali <anjalip@iitbhilai.ac.in>
2024-06-05 19:22:49 +0530

    property model added

---
+++ backend/models/property.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const propertySchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  price: { type: Number, required: true },
+  Bhk: { type: Number },
+  area: { type: Number },
+  //type:Residential, Commercial
+  type: { type: String, required: true },
+  //status:Available, Sold, Rented
+  status: { type: String },
+  //purpose:Buy, Rent,PG/Coliving
+  purpose: {
+    type: String,
+  },
+  amenities: { type: [String], default: [] },
+  created_at: { type: Date, default: Date.now },
+});
+
+const Property = mongoose.model("Property", propertySchema);
+
+module.exports = Property;
